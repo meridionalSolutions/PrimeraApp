7e6a39c567676e41f075c57b56c45e697dcb42f7
Meridional Solutions <meridionalsolutions@gmail.com>
2017-06-06 17:57:14 -0500

    Initial commit

---
+++ app/src/main/java/com/meridionalsolutions/primeraapp/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.meridionalsolutions.primeraapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Usuario on 6/6/2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+
+
+}
